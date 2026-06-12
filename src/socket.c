638c25d9438f34f50e38177e7e5d014a7f52909c
deradera0305 <dera200235@icloud.com>
2026-06-12 21:53:28 +0900

    socket_server_init()の続きとsocket_accept()作成

--- src/socket.c
+++ src/socket.c
@@ -32,7 +32,31 @@ int socket_server_init(int port){
         return -1; // ポート割り当て失敗
     }
 
-    // 4, listen()で接続待ち状態にする
+    // 4.listen()で接続待ち状態にする
+    // 第2引数　5: 待ち受けキューの最大数（今回は5に設定する）
+    if(listen(server_fd, 5) < 0){
+        close(server_fd);
+        return -1; // 待ち受け状態への移行失敗
+    }
 
-};
+    // 5.成功したらserver_fdを返す
+    return server_fd;
+}
 
+int socket_accept(int server_fd){
+    // 1.クライアントからの接続を受けとるための構造体を用意する
+    struct sockaddr_in client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+    // 2.接続を受け入れる（クライアントが来るまでブロックする）
+    // server_fd : 待ち受け用ソケット
+    // client_addr : クライアントのIP/portが入る
+    // addr_len : 構造体のサイズ
+    int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &addr_len);
+    if(client_fd < 0){
+        return -1; // 受け入れ失敗
+    }
+
+    // 3.通信用のソケット番号を返す
+    return client_fd;
+}
